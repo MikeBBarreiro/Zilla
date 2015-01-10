046358cfa6bf8ab3cc81f1753fee69be17d59efd
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2015-01-10 17:25:08 -0600

    text

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,4 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
 game.state.add('menu', menu);
 game.state.add('level', level);
 game.state.add('win', win);
-game.state.start('menu');
+game.state.start('win');
--- public/js/win.js
+++ public/js/win.js
@@ -6,7 +6,7 @@ var win = (function(){
   };
 
   function preload(){
-    game.load.image('restart', '/assets/img/restart.jpeg');
+    // game.load.image('restart', '/assets/img/restart.jpeg');
     game.load.image('backdrop', '/assets/img/night.png');
     game.load.audio('level1Music', '/assets/audio/Zilla.mp3');
     game.load.audio('awesomeness', '/assets/audio/Roar.mp3');
@@ -23,12 +23,13 @@ var win = (function(){
 
     backdrop.autoScroll(-20, 0);
 
-    button2 = game.add.button(game.world.centerX - 95, 400, 'restart', restartLevel, this);
+    // button2 = game.add.button(game.world.centerX - 95, 400, 'restart', restartLevel, this);
 
     level1Music = game.add.audio('level1Music', 1, true);
     Roar = game.add.audio('awesomeness', 0.5, false);
 
     winner = game.add.text(275, 300, 'You Win! try again?', { fontSize: '32px', fill: '#4B6EEF' });
+    winner = game.add.text(255, 400, 'restart the page to play', { fontSize: '32px', fill: '#4B6EEF' });
 
     Roar.play();
     // level1Music.play();
