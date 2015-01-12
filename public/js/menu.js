c195ef8894da5a564942cb4f7c202d692002b984
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2015-01-11 18:41:54 -0600

    changed menu backdrop

--- public/js/menu.js
+++ public/js/menu.js
@@ -7,17 +7,22 @@ var menu = (function(){
 
   function preload(){
     game.load.spritesheet('button', '/assets/img/button_sprite_sheet.png', 193, 71);
-    game.load.image('backdrop', '/assets/img/menu.jpg');
+    game.load.image('backdrop', '/assets/img/godzillaBackgroundMenu.jpg');
     game.load.audio('level1Music', '/assets/audio/Zilla.mp3');
     game.load.audio('awesomeness', '/assets/audio/Roar.mp3');
   }
 
-  var button;
-  var firstMenu = true;
+  var button,
+      MenuBackground,
+      firstMenu = true;
 
   function create(){
 
-    game.add.sprite(0, 0, 'backdrop');
+    MenuBackground = game.add.sprite(0, 0, 'backdrop');
+    MenuBackground.scale.setTo(0.42 , 0.5);
+    // MenuBackground.anchor.setTo(0.5, 0.5);
+    // MenuBackground.body.setSize(40,80);
+
     button = game.add.button(game.world.centerX - 95, 400, 'button', startClick, this, 2, 1, 0);
 
     level1Music = game.add.audio('level1Music', 1, true);
